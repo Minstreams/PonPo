493f1a8b1e9634797d4cab27172b73aeb827d9f1
Minstreams <minstreams@qq.com>
2019-04-24 15:11:42 +0800

    旋转刀片和挥舞刀片

---
+++ Assets/Scripts/Rotating.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : Prop
+{
+    public float speed = 30;
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+
+    protected override void Restart()
+    {
+        base.Restart();
+        transform.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/Waving.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waving : Prop
+{
+    public float range;
+    public float speed;
+    public float startTimer;
+
+    private float timer = 0;
+    private void Update()
+    {
+        timer += Time.deltaTime * speed * Mathf.PI / 2f;
+        float angle = range * Mathf.Sin(timer);
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        timer = startTimer * Mathf.PI / 2f;
+    }
+
+    protected override void Restart()
+    {
+        base.Restart();
+        timer = startTimer * Mathf.PI / 2f;
+    }
+}
